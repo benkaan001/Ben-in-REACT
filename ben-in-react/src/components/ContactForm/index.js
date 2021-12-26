af448e2dafc53e530d5919d327c2f9ac0f136f66
Ben Kaan <benkaan001@gmail.com>
2021-12-26 02:28:27 -0600

    create contact form and add emailhelper util

--- ben-in-react/src/components/ContactForm/index.js
+++ ben-in-react/src/components/ContactForm/index.js
@@ -1,10 +1,44 @@
 import React from 'react';
+import { validateEmail } from '../../utils/emailHelper';
+
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 const ContactForm = () => {
   return (
-    <div>
-      <h2>contact form </h2>
-    </div>
+    <>
+      <Form id='contactForm'>
+        <Form.Group controlId='name'>
+          <Form.Label>Name</Form.Label>
+          <Form.Control required name='name' placeholder='your name' />
+        </Form.Group>
+        <Form.Group controlId='email'>
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            required
+            name='email'
+            type='email '
+            placeholder='email@gmail.com'
+          />
+        </Form.Group>
+        <Form.Group controlId='subject'>
+          <Form.Label>Subject</Form.Label>
+          <Form.Control required name='subject' placeholder='subject' />
+        </Form.Group>
+        <Form.Group controlId='message'>
+          <Form.Label>Message</Form.Label>
+          <Form.Control
+            required
+            name='message'
+            rows='6'
+            as='textarea'
+            placeholder='your message'
+          />
+        </Form.Group>
+
+        <Button type='submit'>Submit</Button>
+      </Form>
+    </>
   );
 };
 
--- ben-in-react/src/pages/Contact.js
+++ ben-in-react/src/pages/Contact.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import ContactForm from '../components/ContactForm';
 
 const Contact = () => {
   return (
     <>
-      <h2>contact page</h2>
+      <ContactForm />
     </>
   );
 };
