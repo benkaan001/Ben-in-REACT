9b5814cc8fe65801a093b1511e3d844c0b489de4
benkaan001 <benkaan001@gmail.com>
2021-12-27 15:46:02 -0600

    add redirect feature to HomePage

--- ben-in-react/src/pages/HomePage.js
+++ ben-in-react/src/pages/HomePage.js
@@ -1,25 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Typewriter from 'typewriter-effect';
 
-const HomePage = () => {
+import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image';
+
+const HomePage = (props) => {
+  const [redirect, setRedirect] = useState(false);
+
+  const typeHandler = async () => {
+    window.setTimeout(() => {
+      setRedirect(true);
+    });
+    window.setTimeout(() => {
+      props.history.push('/about');
+    }, 1000);
+  };
   return (
-    <div className='App'>
-      <Typewriter
-        onInit={(typewriter) => {
-          typewriter
+    <>
+      <Container>
+        <Container fluid className='p-0 mb-0 jumbotron'>
+          <Container className='p-md-3 text-center typewriter'>
+            <Typewriter
+              onInit={(typewriter) => {
+                typewriter
 
-            .typeString('Hello World!')
-            .pauseFor(1000)
-            .changeDeleteSpeed(100)
-            .deleteAll()
-            .typeString('meetBen();')
-            .pauseFor(1000)
+                  .typeString('> Hello, world!')
+                  .pauseFor(1000)
+                  .deleteAll()
+                  .typeString('> meetBen();')
+                  .pauseFor(2000)
+                  .deleteAll()
+                  .typeString('> Redirecting you to ...')
+                  .callFunction(() => typeHandler())
+                  .start();
+              }}
+            />
+          </Container>
+        </Container>
 
-            .deleteAll()
-            .start();
-        }}
-      />
-    </div>
+        <Container>
+          <Image
+            src={require('../assets/project.JPG')}
+            thumbnail
+            width='3300vw'
+            height='3300vw'
+          />
+        </Container>
+      </Container>
+    </>
   );
 };
 
